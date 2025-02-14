1e5f359e8eba99c2043ed1e81bd020ed04838040
ruben.rafaelmartinez <ruben.rafaelmartinez@cm.com>
2025-02-14 11:58:05 +0100

    minor doc comments update

--- topics/go/design/error_handling/example1/mychanges/example1.go
+++ topics/go/design/error_handling/example1/mychanges/example1.go
@@ -1,4 +1,4 @@
-// Ruben: These changes were made in the video of safaribooks but not in this code, so I added it.
+// Ruben: These changes were made in the video of safaribooks but not in this code, so I added this file.
 
 // All material is licensed under the Apache License Version 2.0, January 2004
 // http://www.apache.org/licenses/LICENSE-2.0
